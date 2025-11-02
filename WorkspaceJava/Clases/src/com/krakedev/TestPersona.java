b3553893aafc884c5dafb8cbb23bd77d90048ee3
LuisLlumiquinga <lmllumiquinga3@espe.edu.ec>
2025-11-02 16:44:26 -0500

    Actualizacion PC

---
+++ WorkspaceJava/Clases/src/com/krakedev/TestPersona.java
@@ -0,0 +1,37 @@
+package com.krakedev;
+
+public class TestPersona {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Persona p; // 1. Declaro una variable llamada p de tipo persona
+		Persona p2 = new Persona();
+		p = new Persona(); // 2. Instanciar un objeto Persona, referenciarlo con p
+
+		// 3. Accedo a los atributos
+
+		System.out.println("nombre: " + p.getNombre());
+		System.out.println("edad: " + p.getEdad());
+		System.out.println("estatura: " + p.getEstatura());
+
+		// 4. Modificar los atributos
+		p.setNombre("Mario");
+		p.setEdad(45);;
+		p.setEstatura(1.56);
+
+		System.out.println("--------------");
+
+		// 5. Accedo a los a los atributos
+		System.out.println("nombre: " + p.getNombre());
+		System.out.println("edad: " + p.getEdad());
+		System.out.println("estatura: " + p.getEstatura());
+
+		p2.setNombre("Angelina");
+
+		System.out.println("\n*********");
+		System.out.println("p.nombre: " + p.getNombre());
+		System.out.println("p2.nombre: " + p2.getNombre());
+	}
+
+}

302ecc74bcac467460f107d2b49109d5159d6794
Luis Miguel Llumiquinga Llumiquinga <121265247+LuisLlumiquinga@users.noreply.github.com>
2026-05-13 10:09:13 -0500

    Taller Artesanales, OK

--- WorkspaceJava 2/Artesanal/src/com/krakedev/artesanal/testNegocio/TestNegocio.java
+++ WorkspaceJava 2/Artesanal/src/com/krakedev/artesanal/testNegocio/TestNegocio.java
@@ -2,6 +2,7 @@ package com.krakedev.artesanal.testNegocio;
 
 import com.krakedev.artesanal.Maquina;
 import com.krakedev.artesanal.Negocio;
+import com.krakedev.artesanal.NegocioMejorado;
 
 public class TestNegocio {
 
@@ -15,6 +16,10 @@ public class TestNegocio {
 		Maquina m1 = negocio1.getMaquinaA();
 		double capacidad = m1.getCapacidadMaxima();
 
+		//Test negocio mejorado
+		NegocioMejorado n1=new NegocioMejorado();
+		String codigo=n1.generarCodigo();
+		System.out.println("Codigo negocio mejorado: "+codigo);
 	}
 
 }
